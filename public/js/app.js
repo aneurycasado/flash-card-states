85c63db5781a7892367c25bdbb8052aa6c6fedc1
Aneury Casado <aneury.casado@gmail.com>
2015-08-31 13:42:14 -0400

    Cool

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,17 @@
-var app = angular.module('flashCards', []);
+var app = angular.module('flashCards', ['ui.router']);
+
+app.config(function($stateProvider){
+  $stateProvider
+  .state('scoreBoard', {
+    url: "/#/scoreBoard",
+    templateUrl: "/js/directives/ui-view/scoreBoard.html"
+  })
+  .state('allFlashCards', {
+    url: "/#/allFlashCards",
+    templateUrl: "/js/directives/ui-view/allFlashCards.html",
+  })
+  .state('addNewFlashCard', {
+    url: "/#/addNewFlashCard",
+    templateUrl: "/js/directives/ui-view/addNewFlashCard.html"
+  })
+});
